fefc3fb3685f62000958fd1ba0f714b7b29e0782
Dejice Jacob <dejice.jacob@glasgow.ac.uk>
2025-04-15 17:20:17 +0100

    Naive Interpreter

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -1,24 +1,33 @@
 use std::env; 
 
-extern crate interpreter; 
-use interpreter::interpreter as interp;
-mod utils {
-    pub fn read_file(file_name: &str) -> Result<Vec<u8>> { 
 
+extern crate interpreter; 
+const HEAPSIZE: usize = 2 * 1024; 
+
+mod parser {
+    pub fn new_program(bytestream: &[u8], heapsz: usize) -> interpreter::Program_State { 
+        interpreter::Program_State::new(bytestream, heapsz) 
     }
 }
 
-
-
-fn main() {
+fn main() -> std::io::Result<()> {
     if let Some(prog_name) = env::args().peekable().peek() { 
         println!("program name -> {}", prog_name);
+        println!("================");
     }
 
     for (itr, arg) in env::args().skip(1).enumerate() {
-        println!("Arg[{}] -> {}", itr, arg);
-        if let Ok(ret) = interp::interpret(&arg) { 
-            println!("interpreter returned {}" , ret);
+        //println!("Arg[{}] -> {}", itr, arg);
+        let buffer = std::fs::read(&arg)?; 
+        let mut prog = parser::new_program(&buffer, HEAPSIZE);
+        //println!("buffer = {:?}", prog.txt);
+
+        /* Interpret the program */
+        if let Ok(ret) = prog.interpret() {
+            println!("\n============");
+            println!("interpreter returned {}", ret);
+            println!("=================");
         }; 
     }
+    Ok(())
 }
--- interpreter/src/lib.rs
+++ interpreter/src/lib.rs
@@ -1,8 +1,108 @@
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+pub enum BF_ISA { 
+    Incr, 
+    Decr, 
+    Out,
+    In,
+    MvRight,
+    MvLeft,
+    Jmp,
+    Ret
+}
 
-pub mod interpreter {
-    pub fn interpret(bytestream: &str) -> Result<u32,u32> {
-        println!("{:?}", bytestream);
-        Ok(1)
+pub struct Program_State { 
+    ptr:  usize,
+    pc:   usize,
+    heap: Vec<u8>, 
+    txt:  Vec<BF_ISA>
+}
+
+impl Program_State { 
+    pub fn new(src: &[u8], heap_sz: usize) -> Program_State { 
+        let code: Vec<BF_ISA> = src.into_iter().filter_map( |byte| { 
+            match byte { 
+                b'+' => Some(BF_ISA::Incr),
+                b'-' => Some(BF_ISA::Decr),
+                b'.' => Some(BF_ISA::Out),
+                b',' => Some(BF_ISA::In),
+                b'>' => Some(BF_ISA::MvRight),
+                b'<' => Some(BF_ISA::MvLeft),
+                b'[' => Some(BF_ISA::Jmp),
+                b']' => Some(BF_ISA::Ret),
+                _    => None 
+            }
+        }).collect(); 
+
+        Program_State { 
+            ptr:  0, 
+            pc:   0, 
+            heap: vec![0; heap_sz], 
+            txt:  code,
+        } 
+    }
+
+    pub fn interpret(&mut self) -> Result<i32, &'static str> {
+        'program: loop { 
+            match self.txt[self.pc] { 
+                BF_ISA::Incr => self.heap[self.ptr] = self.heap[self.ptr].wrapping_add(1),
+                BF_ISA::Decr => self.heap[self.ptr] = self.heap[self.ptr].wrapping_sub(1),
+                BF_ISA::Out => print!("{}", self.heap[self.ptr] as char),
+                BF_ISA::In => { 
+                    use std::io::Read; 
+                    let _ = match std::io::stdin().read_exact(&mut self.heap[self.ptr..self.ptr+1]) { 
+                        Ok(()) => 0, 
+                        Err(_) => { return Err("Error reading from stdio"); }, 
+                    };
+                }, 
+                BF_ISA::MvRight => self.ptr = (self.ptr+1) % self.heap.len(), 
+                BF_ISA::MvLeft => self.ptr = (self.ptr + self.heap.len() - 1) % self.heap.len(),
+                BF_ISA::Jmp => { 
+                    if self.heap[self.ptr] == 0 { 
+                        let mut nesting = 1; 
+                        loop { 
+                            if self.pc + 1 == self.txt.len() { 
+                                break 'program; 
+                            } 
+                            self.pc += 1; 
+                            nesting += match self.txt[self.pc] { 
+                                BF_ISA::Jmp => 1,
+                                BF_ISA::Ret => -1, 
+                                _ => 0, 
+                            }; 
+                            if nesting == 0 { 
+                                break; 
+                            } 
+                        }
+                    }
+                },
+                BF_ISA::Ret => { 
+                    if self.heap[self.ptr] != 0 { 
+                        let mut nesting = 1; 
+                        loop { 
+                            if self.pc == 0 { 
+                                break 'program; 
+                            } 
+                            self.pc -= 1; 
+                            nesting += match self.txt[self.pc] { 
+                                BF_ISA::Jmp => -1,
+                                BF_ISA::Ret => 1, 
+                                _ => 0, 
+                            }; 
+                            if nesting == 0 { 
+                                break; 
+                            } 
+                        }
+                    }
+                }
+            }
+
+            self.pc += 1;
+
+            if self.txt.len() == self.pc {
+                break 'program;
+            }
+        }
+        Ok(0)
     }
 }
 
